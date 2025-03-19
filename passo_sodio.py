110b70ea16072ff4994ec36546c69d7dae7f93d0
Alice Arena <a.arena18@campus.unimib.it>
2025-03-19 10:18:33 +0100

    Update passo_sodio.py

--- passo_sodio.py
+++ passo_sodio.py
@@ -35,25 +35,33 @@ def main ():
     n = 1
     
     ''' lunghezze d'onda Na verde e arancione'''
-    lambda_v = 520 * 10**(-9)
+    lambda_v = 498 * 10**(-9)
     d_lamda_v = 0.1 * 10**(-9)
-    lambda_a = 589 * 10**(-9)
+    lambda_a = 615 * 10**(-9)
     d_lamda_a = 0.1 * 10**(-9)
-    theta_v = 0.5 * (angoli_rad(168-89, 30-11))
-    theta_a = 0.5 * (angoli_rad(173-79, 17-40))
+    lambda_g = 589 * 10**(-9)
+    d_lamda_g = 0.1 * 10**(-9)
+    
+    theta_v = 0.5 * (angoli_rad(239-167, 45-5))
+    theta_a = 0.5 * (angoli_rad(248-159, 16-10))
+    theta_g = 0.5 * (angoli_rad(246-161, 10))
     
     passo_v = passo(n, lambda_v, theta_v)
     passo_a = passo(n, lambda_a, theta_a)
+    passo_g = passo(n, lambda_g, theta_g)
     
-    d_passo_v = errore_passo(n, lambda_v, theta_v, d_lamda_v, 0.5 * angoli_rad(1, 0))
-    d_passo_a = errore_passo(n, lambda_a, theta_a, d_lamda_a, 0.5 * angoli_rad(1, 0))
+    d_passo_v = errore_passo(n, lambda_v, theta_v, d_lamda_v, 0.5 * angoli_rad(0, 3))
+    d_passo_a = errore_passo(n, lambda_a, theta_a, d_lamda_a, 0.5 * angoli_rad(0, 3))
+    d_passo_g = errore_passo(n, lambda_g, theta_g, d_lamda_g, 0.5 * angoli_rad(0, 3))
     
     print ("Passo verde: ", passo_v, "m")
     print ("Errore passo verde: ", d_passo_v, "m")
     print ("Passo arancione: ", passo_a, "m")
     print ("Errore passo arancione: ", d_passo_a, "m")
+    print ("Passo giallo: ", passo_g, "m")
+    print ("Errore passo giallo: ", d_passo_g, "m")
     
-    print ("Compatibilità: ", compatibilita(passo_v, passo_a, d_passo_v, d_passo_a))
-    
+    '''print ("Compatibilità: ", compatibilita(passo_v, passo_a, d_passo_v, d_passo_a))
+     '''   
 if __name__ == "__main__":
     main()
